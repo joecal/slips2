2e767033fa66ac8d58984c2fb48abe69c0c472f6
Joe Calvillo <jcalvillo757@gmail.com>
2016-07-06 21:29:39 -0400

    Added logic to prevent being asked a question already answered. Refactored a bit.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,7 @@
 class UsersController < ApplicationController
 
+  helper_method :random_question
+
   def index
     @users = User.all
   end
@@ -11,7 +13,7 @@ class UsersController < ApplicationController
   def create
     @user = User.create(user_params)
     session[:user_id] = @user.id
-    redirect_to users_path(@user)
+    redirect_to @user
   end
 
   def show
@@ -28,6 +30,21 @@ class UsersController < ApplicationController
     redirect_to root_url unless @current_user == @user
     @user.update(user_params)
     redirect_to users_path(@user)
+  end
+
+  def random_question
+   subject1 = Subject.find(1)
+   get_random_question = subject1.questions.sample
+   @question = get_random_question
+
+   answer_user_id_array = @question.answers.collect(&:user_id)
+
+   if answer_user_id_array.include? @current_user.id
+     get_random_question = subject1.questions.sample
+     @question = get_random_question
+   else
+   end
+    @question
   end
 
   private
