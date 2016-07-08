ac9b94c78128ef6e0fe81c81edb991d463f50209
Joe Calvillo <jcalvillo757@gmail.com>
2016-07-08 09:47:45 -0400

    Fixed app controller error

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -14,29 +14,29 @@ class ApplicationController < ActionController::Base
     end
   end
 
-  # def random_question
-  #
-  #   td = Date.today
-  #   todays_date = td.strftime('%Y/%m/%d')
-  #
-  #   if Date.today == Date.between?(2016-07-06..2016-07-08)
-  #     subject1 = Subject.find(1)
-  #     get_random_question = subject1.questions.sample
-  #     @question = get_random_question
-  #   else
-  #   end
+  def random_question
+
+    # td = Date.today
+    # todays_date = td.strftime('%Y/%m/%d')
+    #
+    # if Date.today == Date.between?(2016-07-06..2016-07-08)
+    #   subject1 = Subject.find(1)
+    #   get_random_question = subject1.questions.sample
+    #   @question = get_random_question
+    # else
+    # end
 
   #  subject1 = Subject.find(1)
   #  get_random_question = subject1.questions.sample
-  #  get_random_question = Question.all.sample
-  #  @question = get_random_question
+   get_random_question = Question.all.sample
+   @question = get_random_question
 
    answer_user_id_array = @question.answers.collect(&:user_id)
 
    if answer_user_id_array.include? @current_user.id
-     subject1 = Subject.find(1)
-     get_random_question = subject1.questions.sample
-        #  get_random_question = Question.all.sample
+    #  subject1 = Subject.find(1)
+    #  get_random_question = subject1.questions.sample
+     get_random_question = Question.all.sample
      @question = get_random_question
    else
    end
